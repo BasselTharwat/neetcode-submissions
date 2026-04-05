86a91c5974a6fc7528c05663855e7390a73e7185
Bassel Tharwat <120306721+BasselTharwat@users.noreply.github.com>
2026-04-05 18:37:11 +0200

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = [0 for i in range(len(nums))]
+        zeroFound = False
+        zeroFoundIndex = 0
+        prod = 1
+        
+        # find zeros. one zero = result has one value only. multiple zeros = result is all zeros
+        # create frequency dictionary
+        for i in range(len(nums)):
+            if nums[i] == 0 and zeroFound == True:
+                return result
+            elif nums[i] == 0:
+                zeroFound = True
+                zeroFoundIndex = i
+            else:
+                prod *= nums[i]
+
+        # fill in results array
+        for j in range(len(nums)):
+            if zeroFound == True and j == zeroFoundIndex:
+                result[j] = prod
+            elif zeroFound == False:
+                result[j] = prod // nums[j]
+            
+        return result
+
+            
+                
+            
+
+
+
+        
